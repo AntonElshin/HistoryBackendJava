e2ae8c84b9f3aa5bda39ebd856b57654f5491a31
Anton Elshin <anton.elshin@mail.ru>
2025-02-23 17:11:45 +0300

    fill DML operations

---
+++ files/DML.sql
@@ -0,0 +1,29 @@
+-- DML (Data Management Language)
+
+-- selects
+
+select * from race;
+select * from distance;
+select * from registration;
+
+-- inserts into race
+
+insert into race (id, race_date, race_name, race_city, race_address, description) values
+    (1, to_date('20250222', 'yyyymmdd'), 'Забег настоящих мужчин', 'Москва', 'Перовский сквер', null);
+
+-- inserts into distance
+
+insert into distance (id, distance_name, entrance_fee, racer_limit) values
+    (1, 'FULL', 1400.00, null);
+
+-- inserts into registration
+
+insert into registration (id, last_name, first_name, middle_name, birth_date, gender, email, mobile_phone, is_paid) values
+    (1, 'Петров', 'Василий', 'Александрович', to_date('19850417', 'yyyymmdd'), 'MALE', 'petrov_va@mail.ru', '9999999999', false);
+
+-- deletes
+
+delete from race;
+delete from distance;
+delete from registration;
+
